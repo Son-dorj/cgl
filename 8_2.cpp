0cf02c547c7a541025cae342b44a560e69bb38d1
Son-dorj <94377854+Son-dorj@users.noreply.github.com>
2022-07-04 09:49:40 +0530

    Update 8_2.cpp

--- 8_2.cpp
+++ 8_2.cpp
@@ -1,169 +1,151 @@
-#include<GL/glut.h>
+#include"stdafx.h"
+#include<glut.h>
 #include<stdio.h>
-#include<stdlib.h>
 #include<math.h>
 #define PI 3.1416
-static int win,val=0,CMenu;
-void CreateMenu(void);
-void Menu(int value);
-struct wcPt3D
+
+float theta = 0;
+
+struct point
 {
-	GLfloat x,y,z;
+    GLfloat x, y, z;
 };
-GLsizei winWidth=600,winHeight=600;
-GLfloat xwcMin=0.0,xwcMax=130.0;
-GLfloat ywcMin=0.0,ywcMax=130.0;
-void bino(GLint n,GLint *C)
+
+int factorial(int n)
 {
-	GLint k,j;
-	for(k=0;k<=n;k++)
-	{
-		C[k]=1;
-		for(j=n;j>=k+1;j--)
-		C[k]*=j;
-		for(j=n-k;j>=2;j--)
-		C[k]/=j;
-	}
+    if (n<=1)
+        return(1);
+    else
+        n=n*factorial(n-1);
+    return n;
 }
-void computeBezPt(GLfloat u,struct wcPt3D *bezPt,GLint nCtrlPts,struct wcPt3D *ctrlPts,GLint *C)
+
+void computeNcR(int n, int *hold_ncr_values)
 {
-	GLint k,n=nCtrlPts-1;
-	GLfloat bezBlendFcn;
-	bezPt->x =bezPt->y =bezPt->z=0.0;
-	for(k=0;k<nCtrlPts;k++)
-	{
-		bezBlendFcn=C[k]*pow(u,k)*pow(1-u,n-k);
-		bezPt->x += ctrlPts[k].x * bezBlendFcn;
-		bezPt->y += ctrlPts[k].y * bezBlendFcn;
-		bezPt->z += ctrlPts[k].z * bezBlendFcn;
-	}
+    int r;
+    for(r=0; r<=n; r++) //start from nC0, then nC1, nC2, nC3 till nCn
+    {
+        hold_ncr_values[r] = factorial(n) / (factorial(n-r) * factorial(r));
+    }
 }
-void bezier(struct wcPt3D *ctrlPts, GLint nCtrlPts,GLint nBezCurvePts)
+
+void computeBezierPoints(float t, point *actual_bezier_point, int number_of_control_points, point *control_points_array, int *hold_ncr_values)
 {
-	struct wcPt3D bezCurvePt;
-	GLfloat u;
-	GLint *C,k;
-	C=new GLint[nCtrlPts];
-	bino(nCtrlPts-1,C);
-	glBegin(GL_LINE_STRIP);
-	for(k=0;k<=nBezCurvePts;k++)
-	{
-		u=GLfloat(k)/GLfloat(nBezCurvePts);
-		computeBezPt(u,&bezCurvePt,nCtrlPts,ctrlPts,C);
-		glVertex2f(bezCurvePt.x, bezCurvePt.y);
-	}
-glEnd();
-delete[]C;
+    int i, n = number_of_control_points-1;
+
+    float bernstein_polynomial;
+
+    actual_bezier_point -> x = 0;
+    actual_bezier_point -> y = 0;
+    actual_bezier_point -> z = 0;
+
+    for(i=0; i < number_of_control_points; i++)
+    {
+        bernstein_polynomial = hold_ncr_values[i] * pow(t, i) * pow( 1-t, n-i);
+
+        actual_bezier_point -> x += bernstein_polynomial * control_points_array[i].x;
+        actual_bezier_point -> y += bernstein_polynomial * control_points_array[i].y;
+        actual_bezier_point -> z += bernstein_polynomial * control_points_array[i].z;
+    }
 }
-void displayFcn()
+
+void bezier(point *control_points_array, int number_of_control_points, int number_of_bezier_points)
 {
-	GLint nCtrlPts=4,nBezCurvePts=20;
-	static float theta=0;
-	struct wcPt3D ctrlPts[4]={{20,100,0},{30,110,0},{50,90,0},{60,100,0}};
-	ctrlPts[1].x +=10*sin(theta * PI/180.0);
-	ctrlPts[1].y +=5*sin(theta * PI/180.0);
-	ctrlPts[2].x -=10*sin((theta+30) * PI/180.0);
-	ctrlPts[2].y -=10*sin((theta+30) * PI/180.0);
-	ctrlPts[3].x -=4*sin((theta)*PI/180.0);
-	ctrlPts[3].y += sin((theta-30)*PI/180.0);
-	theta+=0.1;
-	glClear(GL_COLOR_BUFFER_BIT);
-	glColor3f(0.0,0.0,0.0);
-	glPointSize(5);
-	if(val==1){
-	glPushMatrix();
-	glLineWidth(5);
-	glColor3f(1.0,0.5,0);
-	for(int i=0;i<8;i++)
-	{
-		glTranslatef(0,-0.8,0);
-		bezier(ctrlPts, nCtrlPts, nBezCurvePts);
-	}
-	glColor3f(1,1,1);
-	for(int i=0;i<8;i++)
-	{
-		glTranslatef(0,-0.8,0);
-		bezier(ctrlPts, nCtrlPts, nBezCurvePts);
-	}
-	glColor3f(0,1.0,0);
-	for(int i=0;i<8;i++)
-	{
-		glTranslatef(0,-0.8,0);
-		bezier(ctrlPts, nCtrlPts, nBezCurvePts);
-	}
-		glPopMatrix();
-		glColor3f(0.7,0.5,0.3);
-		glLineWidth(5);
-		glBegin(GL_LINES);
-		glVertex2f(20,100);
-		glVertex2f(20,40);
-		glEnd();
-		glFlush();
-	}
-	if(val==2){
-	glPushMatrix();
-	glLineWidth(5);
-	glColor3f(1.0,1.0,0.0);
-	for(int i=0;i<12;i++)
-	{
-		glTranslatef(0,-0.8,0);
-		bezier(ctrlPts, nCtrlPts, nBezCurvePts);
-	}
-	glColor3f(1,0.0,0.0);
-	for(int i=0;i<12;i++)
-	{
-		glTranslatef(0,-0.8,0);
-		bezier(ctrlPts, nCtrlPts, nBezCurvePts);
-	}
-	glPopMatrix();
-	glColor3f(0.7,0.5,0.3);
-	glLineWidth(5);
-	glBegin(GL_LINES);
-	glVertex2f(20,100);
-	glVertex2f(20,40);
-	glEnd();
-	glFlush();
-	}
-	glutPostRedisplay();
-	glutSwapBuffers();
+    point actual_bezier_point;
+    float t;
+    int *hold_ncr_values, i;
+
+    hold_ncr_values = new int[number_of_control_points]; // to hold the nCr values
+
+    computeNcR(number_of_control_points-1, hold_ncr_values); // call nCr function to calculate nCr values
+
+    glBegin(GL_LINE_STRIP);
+        for(i=0; i<=number_of_bezier_points; i++)
+        {
+            t=float(i)/float(number_of_bezier_points);
+            computeBezierPoints(t, &actual_bezier_point, number_of_control_points, control_points_array, hold_ncr_values);
+            glVertex2f(actual_bezier_point.x, actual_bezier_point.y);
+        }
+    glEnd();
+
+    delete[] hold_ncr_values;
 }
-void winReshapeFun(GLint newWidth,GLint newHeight)
+
+void display()
 {
-	glViewport(0,0,newWidth,newHeight);
-	glMatrixMode(GL_PROJECTION);
-	glLoadIdentity();
-	gluOrtho2D(xwcMin,xwcMax,ywcMin,ywcMax);
-	glClear(GL_COLOR_BUFFER_BIT);
+    glClear(GL_COLOR_BUFFER_BIT);
+    int number_of_control_points = 4, number_of_bezier_points = 20;
+
+    point control_points_array[4] = {{100, 400, 0}, {150, 450, 0}, {250, 350, 0},{300, 400, 0}};
+
+    control_points_array[1].x += 50*sin(theta * PI/180.0);
+    control_points_array[1].y += 25*sin(theta * PI/180.0);
+
+    control_points_array[2].x -= 50*sin((theta+30) * PI/180.0);
+    control_points_array[2].y -= 50*sin((theta+30) * PI/180.0);
+
+    control_points_array[3].x -= 25*sin((theta-30) * PI/180.0);
+    control_points_array[3].y += sin((theta-30) * PI/180.0);
+
+    theta += 2;
+
+    glPushMatrix();
+
+    glPointSize(5);
+
+    glColor3f(1, 0.4, 0.2); //Indian flag: Saffron color code
+    for(int i=0; i<50; i++)
+    {
+        glTranslatef(0, -0.8, 0 );
+        bezier(control_points_array, number_of_control_points, number_of_bezier_points);
+    }
+
+    glColor3f(1, 1, 1); //Indian flag: white color code
+    for(int i=0; i<50; i++)
+    {
+        glTranslatef(0, -0.8, 0);
+        bezier(control_points_array, number_of_control_points, number_of_bezier_points);
+    }
+
+    glColor3f(0, 1, 0); //Indian flag: green color code
+    for(int i=0; i<50; i++)
+    {
+        glTranslatef(0, -0.8, 0);
+        bezier(control_points_array, number_of_control_points, number_of_bezier_points);
+    }
+
+    glPopMatrix();
+
+    glLineWidth(5);
+
+    glColor3f(0.7, 0.5,0.3); //pole colour
+    glBegin(GL_LINES);
+        glVertex2f(100,400);
+        glVertex2f(100,40);
+    glEnd();
+
+    glutPostRedisplay();
+    glutSwapBuffers();
 }
-void CreateMenu(void)
+
+void init()
 {
-	CMenu=glutCreateMenu(Menu);
-	glutAddMenuEntry("Indian flag",1);
-	glutAddMenuEntry("Karnataka flag",2);
-	glutAddMenuEntry("Exit",0);
-	glutAttachMenu(GLUT_RIGHT_BUTTON);
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
+    gluOrtho2D(0,500,0,500);
 }
-void Menu(int value)
+
+int main(int argc, char **argv)
 {
-	if(value==0)
-	{
-		glutDestroyWindow(win);
-		exit(0);
-	}
-	else
-	{
-		val=value;
-	}
-}
-int main(int argc,char **argv)
-{
-	glutInit(&argc,argv);
-	glutInitDisplayMode(GLUT_DOUBLE|GLUT_RGB);
-	glutInitWindowPosition(50,50);
-	glutInitWindowSize(winWidth,winHeight);
-	glutCreateWindow("prg.8 Bezier Curve: C Tathva");
-	CreateMenu();
-	glutDisplayFunc(displayFcn);
-	glutReshapeFunc(winReshapeFun);
-	glutMainLoop();
+    glutInit(&argc, argv);
+    glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB);
+    glutInitWindowPosition(0, 0);
+    glutInitWindowSize(500,500);
+    glutCreateWindow("Bezier Curve - updated");
+
+    init();
+
+    glutDisplayFunc(display);
+
+    glutMainLoop();
 }
